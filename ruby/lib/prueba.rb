b50e44a92ee493f3e5f0e500067adb4a90a4b10d
Fran <franciscodumont1@gmail.com>
2020-09-19 13:47:22 -0300

    Arrancamos con el tp

--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -1,6 +1,34 @@
+module Contrato
+  attr_accessor :before, :after
+
+  def self.method_added(method_name)
+    alias_method :metodo_viejo, method_name
+    puts "Estoy entrando en #{method_name}"
+    define_method(method_name) do
+      before.call
+      metodo_viejo.call
+      after.call
+    end
+    puts "Estoy saliendo de #{method_name}"
+  end
+
+  def before_and_after_each_call(before, after)
+    @before = before
+    @after = after
+  end
+
+end
+
+
 class Prueba
+  include Contrato
 
   def materia
+    puts "hola"
     :tadp
+  end
+
+  def hola
+    puts "chau"
   end
 end
--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -1,9 +1,18 @@
+require_relative 'spec_helper'
+
 describe Prueba do
   let(:prueba) { Prueba.new }
 
   describe '#materia' do
     it 'debería pasar este test' do
+      prueba.before_and_after_each_call(proc {puts 'Before'}, proc {puts 'After'})
       expect(prueba.materia).to be :tadp
     end
+  end
+end
+
+describe 'Prueba' do
+  it 'asda' do
+    expect(2).to be 2
   end
 end
