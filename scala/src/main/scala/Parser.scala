c405eb4a63ff494a2027346e9063880cd0a6806b
foviedo <oviedoee@yahoo.com>
2020-10-29 09:28:45 -0300

    monster commit, falta double de los primeros parsers

---
+++ scala/src/main/scala/Parser.scala
@@ -0,0 +1,54 @@
+import scala.util.{Failure, Success, Try}
+
+
+
+abstract class Parser[T] {
+  def aplicar(entrada:String): Try[ResultadoParser[T]]
+
+}
+
+case object AnyChar extends Parser[Char] {
+  def aplicar(unString: String): Try[ResultadoParser[Char]] = unString.toList match {
+    case List() => Failure (new StringVacioException)
+    case head :: tail => Success (new ResultadoParser(head))
+  }
+}
+
+case object IsDigit extends Parser[Char] {
+  def aplicar(digitoEnString:String): Try[ResultadoParser[Char]] =
+    Try {
+      if (digitoEnString.toInt <= 9 && digitoEnString.toInt >= 0)
+        new ResultadoParser[Char](digitoEnString.charAt(0))
+      else
+        throw new IsDigitException
+    }
+}
+
+case class char(inicio: Char) extends Parser[Char]{
+  def aplicar(unString: String): Try[ResultadoParser[Char]] = AnyChar.aplicar(unString) match {
+    case Failure(_) =>  Failure(new StringVacioException)
+    case Success(ResultadoParser(unChar)) if unChar!= inicio => Failure(new CharException)
+    case Success(ResultadoParser(unChar)) if unChar == inicio => Success(new ResultadoParser(inicio))
+  }
+}
+
+case class string(inicio: String) extends Parser[String]{
+  def aplicar(stringOriginal: String): Try[ResultadoParser[String]] =
+    if(stringOriginal.startsWith(inicio)) new Success(ResultadoParser[String](inicio)) else Failure(new StringException)
+}
+
+case object integer extends Parser[Int]{
+  def aplicar(entero:String): Try[ResultadoParser[Int]]=
+    Try {
+  ResultadoParser(entero.toInt)
+  }
+}
+
+
+
+class StringVacioException extends Exception
+class CharException extends Exception
+class IsDigitException extends Exception
+class StringException extends Exception
+
+case class ResultadoParser[+T](elementoParseado: T)
--- scala/src/test/scala/ProjectSpec.scala
+++ scala/src/test/scala/ProjectSpec.scala
@@ -1,4 +1,8 @@
+import org.scalatest.TryValues.convertTryToSuccessOrFailure
 import org.scalatest.{FreeSpec, Matchers}
+
+import scala.util.{Failure, Success, Try}
+
 
 class ProjectSpec extends FreeSpec with Matchers {
 
@@ -11,4 +15,64 @@ class ProjectSpec extends FreeSpec with Matchers {
     }
   }
 
+  "anychar" - {
+    "deberia retornar un Success b de banana" in {
+      AnyChar.aplicar("banana").success.value == 'b'
+    }
+    "deberia retornar un failure de string vacio" in {
+      AnyChar.aplicar("").failure.exception shouldBe a[StringVacioException]
+
+    }
+  }
+
+  "char" - {
+    "deberia retornar un success con h de hola" in {
+      char('h').aplicar("hola") shouldBe Success(ResultadoParser('h'))
+    }
+    "deberia darme un char exception porque le pase otra letra" in {
+      char('z').aplicar("hola").failure.exception shouldBe a [CharException]
+    }
+    "deberia retornar un failure de string vacio" in {
+      char('z').aplicar("").failure.exception shouldBe a [StringVacioException]
+
+    }
+  }
+  "isDigit" - {
+    "deberia retornar un 7 re piola" in {
+      IsDigit.aplicar("7") shouldBe Success(ResultadoParser('7'))
+    }
+    "deberia romper con un numero mas grande" in {
+      IsDigit.aplicar("11").failure.exception shouldBe a [IsDigitException]
+    }
+    "deberia romper si te tiro frula" in {
+      IsDigit.aplicar("bro momento").failure.exception shouldBe a [java.lang.NumberFormatException]
+    }
+
+  }
+  "string" - {
+    "deberia romper porque no arranca con mundo" in {
+      string("mundo").aplicar("hola mundo").failure.exception shouldBe a [StringException]
+    }
+
+    "deberia retornar hola porque empieza con hola" in {
+      string("hola").aplicar("hola mundo") shouldBe Success(ResultadoParser("hola"))
+
+    }
+  }
+
+  "integer" - {
+    "deberia funcionar con números positivos" in {
+      integer.aplicar("27") shouldBe Success(ResultadoParser(27))
+    }
+    "deberia funcionar con números negativos" in {
+      integer.aplicar("-27") shouldBe Success(ResultadoParser(-27))
+    }
+    "deberia romper si le tiro frula" in {
+      integer.aplicar("bro momento").failure.exception shouldBe a [java.lang.NumberFormatException]
+    }
+  }
+
+
 }
+
+
