c085459c1ffb7e22753c785cd6b9197f52d8424d
Brian Monroy <brian.gmonroy98@gmail.com>
2020-10-30 01:33:47 -0300

    arreglado lo del string sobrante. listo hasta <|> inclusive

--- scala/src/main/scala/Parser.scala
+++ scala/src/main/scala/Parser.scala
@@ -4,23 +4,23 @@ import scala.util.{Failure, Success, Try}
 abstract class Parser[T] {
   def aplicar(entrada:String): Try[ResultadoParser[T]]
 
-  //def <|> (otroParser:Parser[T]):Parser[T]={
-   // (new <|>).combinar(this,otroParser)
-  //}
+  def <|> (otroParser:Parser[T]):Parser[T]={
+    (new <|>).combinar(this,otroParser)
+  }
 }
 
 case object AnyChar extends Parser[Char] {
   def aplicar(unString: String): Try[ResultadoParser[Char]] = unString.toList match {
     case List() => Failure (new StringVacioException)
-    case head :: tail => Success (new ResultadoParser(head,tail.toString()))
+    case head :: tail => Success (new ResultadoParser(head,tail.mkString("")))
   }
 }
-/*
+
 case object IsDigit extends Parser[Char] {
   def aplicar(digitoEnString:String): Try[ResultadoParser[Char]] =
     Try {
       if (digitoEnString.toInt <= 9 && digitoEnString.toInt >= 0)
-        new ResultadoParser[Char](digitoEnString.charAt(0))
+        new ResultadoParser[Char](digitoEnString.charAt(0),"")
       else
         throw new IsDigitException
     }
@@ -29,27 +29,27 @@ case object IsDigit extends Parser[Char] {
 case class char(inicio: Char) extends Parser[Char]{
   def aplicar(unString: String): Try[ResultadoParser[Char]] = AnyChar.aplicar(unString) match {
     case Failure(_) =>  Failure(new StringVacioException)
-    case Success(ResultadoParser(unChar)) if unChar!= inicio => Failure(new CharException)
-    case Success(ResultadoParser(unChar)) if unChar == inicio => Success(new ResultadoParser(inicio))
+    case Success(ResultadoParser(unChar,resto)) if unChar!= inicio => Failure(new CharException)
+    case Success(ResultadoParser(unChar,resto)) if unChar == inicio => Success(new ResultadoParser(inicio,resto))
   }
 }
 
 case class string(inicio: String) extends Parser[String]{
   def aplicar(stringOriginal: String): Try[ResultadoParser[String]] =
-    if(stringOriginal.startsWith(inicio)) new Success(ResultadoParser[String](inicio)) else Failure(new StringException)
+    if(stringOriginal.startsWith(inicio)) new Success(ResultadoParser[String](inicio,stringOriginal.slice(inicio.length(),stringOriginal.length()))) else Failure(new StringException)
 }
 
 case object integer extends Parser[Int]{
   def aplicar(entero:String): Try[ResultadoParser[Int]]=
     Try {
-  ResultadoParser(entero.toInt)
+  ResultadoParser(entero.toInt,"")
   }
 }
 
 case object double extends Parser[Double]{
   def aplicar(unDouble:String): Try[ResultadoParser[Double]]=
     Try {
-      ResultadoParser(unDouble.toDouble)
+      ResultadoParser(unDouble.toDouble,"")
     }
 }
 
@@ -60,7 +60,7 @@ class <|>[T] {
     new Parser[T]{
       def aplicar(input:String):Try[ResultadoParser[T]]={
         unParser.aplicar(input) match {
-          case Success(ResultadoParser(elem)) => Success(ResultadoParser(elem))
+          case Success(ResultadoParser(head,tail)) => Success(ResultadoParser(head,tail))
           case Failure(_) => otroParser.aplicar(input)
         }
       }
@@ -69,17 +69,20 @@ class <|>[T] {
 
 }
 
+
+/* TODO terminar el <>
 class <>[T,K]{
   def combinar(unParser:Parser[T],otroParser:Parser[K]): Parser[(T,K)] ={
     new Parser[(T, K)] {
       override def aplicar(entrada: String): Try[ResultadoParser[(T, K)]] = {
         val resultadoPrimerParser = unParser.aplicar(entrada)
+        val resultadoSegundoParser = otroParser.aplicar(resultadoPrimerParser.get.loQueSobra)
       }
     }
   }
-}
+}*/
 
-*/
+
 class StringVacioException extends Exception
 class CharException extends Exception
 class IsDigitException extends Exception
--- scala/src/test/scala/ProjectSpec.scala
+++ scala/src/test/scala/ProjectSpec.scala
@@ -8,80 +8,80 @@ import scala.util.{Failure, Success, Try}
 
 class ProjectSpec extends AnyFlatSpec with should.Matchers{
 
+  //shouldBe Success(ResultadoParser('h',"ola")) esto es la posta!
 
     it should "deberia retornar un Success b de banana" in {
-      AnyChar.aplicar("banana").success.value == ('b',"anana")
+      AnyChar.aplicar("banana") shouldBe Success(ResultadoParser('b',"anana"))
     }
-
-
 
     it should "deberia retornar un failure de string vacio" in {
       AnyChar.aplicar("").failure.exception shouldBe a[StringVacioException]
-
     }
-/*
+
 
     it should "deberia retornar un success con h de hola" in {
-      char('h').aplicar("hola") shouldBe Success(ResultadoParser('h'))
-    }
-    it should "deberia darme un char exception porque le pase otra letra" in {
-      char('z').aplicar("hola").failure.exception shouldBe a [CharException]
-    }
-    it should "deberia retornar un failure de string vacio, string" in {
-      char('z').aplicar("").failure.exception shouldBe a [StringVacioException]
-
+      char('h').aplicar("hola") shouldBe Success(ResultadoParser('h',"ola"))
     }
 
-    it should "deberia retornar un 7 re piola" in {
-      IsDigit.aplicar("7") shouldBe Success(ResultadoParser('7'))
-    }
-    it should "deberia romper con un numero mas grande" in {
-      IsDigit.aplicar("11").failure.exception shouldBe a [IsDigitException]
-    }
-    it should "deberia romper si te tiro frula" in {
-      IsDigit.aplicar("bro momento").failure.exception shouldBe a [java.lang.NumberFormatException]
-    }
+      it should "deberia darme un char exception porque le pase otra letra" in {
+        char('z').aplicar("hola").failure.exception shouldBe a [CharException]
+      }
+
+      it should "deberia retornar un failure de string vacio, string" in {
+        char('z').aplicar("").failure.exception shouldBe a [StringVacioException]
+
+      }
+
+      it should "deberia retornar un 7 re piola" in {
+        IsDigit.aplicar("7") shouldBe Success(ResultadoParser('7',""))
+      }
+      it should "deberia romper con un numero mas grande" in {
+        IsDigit.aplicar("11").failure.exception shouldBe a [IsDigitException]
+      }
+      it should "deberia romper si te tiro frula" in {
+        IsDigit.aplicar("bro momento").failure.exception shouldBe a [java.lang.NumberFormatException]
+      }
 
 
-    it should "deberia romper porque no arranca con mundo" in {
-      string("mundo").aplicar("hola mundo").failure.exception shouldBe a [StringException]
-    }
+      it should "deberia romper porque no arranca con mundo" in {
+        string("mundo").aplicar("hola mundo").failure.exception shouldBe a [StringException]
+      }
 
-    it should "deberia retornar hola porque empieza con hola" in {
-      string("hola").aplicar("hola mundo") shouldBe Success(ResultadoParser("hola"))
+      it should "deberia retornar hola porque empieza con hola" in {
+        string("hola").aplicar("hola mundo") shouldBe Success(ResultadoParser("hola"," mundo"))
 
-    }
+      }
 
 
-    it should "deberia funcionar con números positivos" in {
-      integer.aplicar("27") shouldBe Success(ResultadoParser(27))
-    }
-    it should  "deberia funcionar con números negativos" in {
-      integer.aplicar("-27") shouldBe Success(ResultadoParser(-27))
-    }
-    it should "deberia romper si le tiro frula" in {
-      integer.aplicar("bro momento").failure.exception shouldBe a [java.lang.NumberFormatException]
-    }
+        it should "deberia funcionar con números positivos" in {
+          integer.aplicar("27") shouldBe Success(ResultadoParser(27,""))
+        }
+        it should  "deberia funcionar con números negativos" in {
+          integer.aplicar("-27") shouldBe Success(ResultadoParser(-27,""))
+        }
+        it should "deberia romper si le tiro frula" in {
+          integer.aplicar("bro momento").failure.exception shouldBe a [java.lang.NumberFormatException]
+        }
 
-  it should "deberia funcionar con números positivos el double" in {
-    double.aplicar("27.5") shouldBe Success(ResultadoParser(27.5))
-  }
-  it should  "deberia funcionar con números negativos el double" in {
-    double.aplicar("-27.5") shouldBe Success(ResultadoParser(-27.5))
-  }
+      it should "deberia funcionar con números positivos el double" in {
+        double.aplicar("27.5") shouldBe Success(ResultadoParser(27.5,""))
+      }
+      it should  "deberia funcionar con números negativos el double" in {
+        double.aplicar("-27.5") shouldBe Success(ResultadoParser(-27.5,""))
+      }
 
-  it should "deberia andar aunque le mande un entero" in {
-    double.aplicar("5") shouldBe Success(ResultadoParser(5.0))
-  }
+      it should "deberia andar aunque le mande un entero" in {
+        double.aplicar("5") shouldBe Success(ResultadoParser(5.0,""))
+      }
 
-  it should "deberia romper si le tiro frula el double" in {
-    double.aplicar("bro momento").failure.exception shouldBe a [java.lang.NumberFormatException]
-  }
+      it should "deberia romper si le tiro frula el double" in {
+        double.aplicar("bro momento").failure.exception shouldBe a [java.lang.NumberFormatException]
+      }
 
-  it should "el test del tp de <|> deberia andar" in {
-    (char('a') <|> char('b')).aplicar("arbol") shouldBe Success(ResultadoParser('a'))
-  }
-*/
+        it should "el test del tp de <|> deberia andar" in {
+          (char('a') <|> char('b')).aplicar("arbol") shouldBe Success(ResultadoParser('a',"rbol"))
+        }
+
 
 
 }
